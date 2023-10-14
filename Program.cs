2f0dba09f3a3f1237f38ff5a172ebcabab86e3ea
Skyler Adams <skyler14adams@gmail.com>
2023-10-14 14:02:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Student_Registration
+{
+    internal class Program
+    {
+        public static string FirstName { get; private set; }
+        public static string LastName { get; private set; }
+        public static string BirthYear { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Registration Form");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter first name: ");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Enter year of birth: ");
+            BirthYear = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Welcome " + FirstName + " " + LastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + FirstName + "*" + BirthYear);
+        }
+    }
+}
